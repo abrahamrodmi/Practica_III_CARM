fc47c35432aa52d8c4c7fa6aa4099a5005b06d0c
abrahamrodmi <carlos.arodmi31@gmail.com>
2026-04-25 20:16:50 -0600

    modulo corregido

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ReservaModule } from './reserva/reserva.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      //port: 5432,
-      //username: 'root',
-      //password: '',
-      //database: 'reservaciones',
-      autoLoadEntities: true,
-      synchronize: true,
-      ssl: { rejectUnauthorized: false },
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     ReservaModule
